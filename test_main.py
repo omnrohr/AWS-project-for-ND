51d84f76560eb1c169c890c91a2fbe95c9cdadb4
omnrohr <67183475+omnrohr@users.noreply.github.com>
2021-10-13 19:17:30 +0300

    Adding Tests to the Build

--- test_main.py
+++ test_main.py
@@ -17,7 +17,7 @@ def client():
     os.environ['JWT_SECRET'] = SECRET
     main.APP.config['TESTING'] = True
     client = main.APP.test_client()
-
+    
     yield client
 
 
@@ -26,7 +26,7 @@ def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
-
+    assert False
 
 def test_auth(client):
     body = {'email': EMAIL,
